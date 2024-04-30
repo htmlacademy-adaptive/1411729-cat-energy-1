b8c5b32fbdcdfe5e3c819301279419b4b2660a4d
Revuxorn <48528980+AleksanrReva@users.noreply.github.com>
2024-04-30 14:42:11 +0300

    переименовывает минифицированный файл стилей

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
@@ -16,6 +17,7 @@ export const styles = () => {
       autoprefixer(),
       csso()
     ]))
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
